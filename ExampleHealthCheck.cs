628c34549dc1f6d1880de61aa7a1ecc8cac59759
Justin James <digitaldrummerj@gmail.com>
2022-03-12 07:43:59 -0700

    add: example health check with plain text response

---
+++ ExampleHealthCheck.cs
@@ -0,0 +1,29 @@
+namespace AspNetCoreHealthCheckExample;
+
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+public class ExampleHealthCheckAsync : IHealthCheck
+{
+    public Task<HealthCheckResult> CheckHealthAsync(
+        HealthCheckContext context, CancellationToken cancellationToken = default)
+    {
+        try
+        {
+
+            return Task.FromResult(
+
+                HealthCheckResult.Healthy("Health Msg Here."));
+
+        }
+        catch (Exception)
+        {
+
+            return Task.FromResult(
+
+                new HealthCheckResult(
+
+                    context.Registration.FailureStatus, "Unhealth Msg Here."));
+
+        }
+    }
+}
